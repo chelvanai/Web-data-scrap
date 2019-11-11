aa781a8198f32b3c86c7b8bb7b6f828bbdd295f9
chelvan <chelvamchelvan@yahoo.com>
2019-11-11 13:20:05 +0530

    first

---
+++ web_data_extract/spiders/Thesoupspoon.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class ThesoupspoonSpider(scrapy.Spider):
+    name = 'Thesoupspoon'
+    allowed_domains = ["stackoverflow.com/"]
+    start_urls = ['https://stackoverflow.com/questions/tagged/android']
+
+    def parse(self, response):
+        titles = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+
+        for i in titles:
+            print(i)
+
